ec3df8cff319b50d24d9c8af37481ddc58d9edd0
Rafael Ferreira Carvalho Caffonso <rafaelcaffonso@gmail.com>
2025-10-13 13:26:36 -0300

    Definindo rotas da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { CommonModule } from '@angular/common';
-import { PrimaryButtonComponent } from "./components/primary-button/primary-button.component";
-import { SecondaryButtonComponent } from "./components/secondary-button/secondary-button.component";
-import { ItemCertificadoComponent } from "./components/item-certificado/item-certificado.component";
 import { BaseUiComponent } from "./components/base-ui/base-ui.component";
-import { CertificadosComponent } from "./pages/certificados/certificados.component";
-import { CertificadoFormComponent } from "./pages/certificado-form/certificado-form.component";
-import { CertificadoComponent } from "./pages/certificado/certificado.component";
 
 @Component({
   selector: 'app-root',
   imports: [
+    RouterOutlet,
     CommonModule,
     NavbarComponent,
     BaseUiComponent,
-    CertificadoFormComponent,
-    CertificadoComponent
-],
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
